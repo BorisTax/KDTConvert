bad1a24b7865cec72dc1a966dc765b6bccb1e571
BorisTax <tboris1983@gmail.com>
2023-04-10 15:54:40 +0300

    first

--- kdtconvert.py
+++ kdtconvert.py
@@ -6,8 +6,10 @@ from tkinter.scrolledtext import ScrolledText
 from tkinter import Scrollbar
 
 from convert import convert
+from convertToKDT import convertToKDT
 
 class MyApp():
+
     def __init__(self) -> None:
         self.root = tk.Tk()
         self.root.title('KDT конвертер')
@@ -15,50 +17,60 @@ class MyApp():
         self.root.geometry('500x200')
         self.root.eval('tk::PlaceWindow . center')
         self.frame = ttk.Frame(self.root)
-        self.open_button = ttk.Button(
-            self.frame,
-            text='Выбрать файлы',
-            command=self.select_file
-        )
-        self.help_button = ttk.Button(
-            self.frame,
-            text='?',
-            width=3,
-            command=self.help
-        )
-        h=Scrollbar(self.root, orient='horizontal')
+        self.open_button = ttk.Button(self.frame,
+                                      text='Выбрать файлы',
+                                      command=self.select_file_xnc)
+        self.help_button = ttk.Button(self.frame,
+                                      text='?',
+                                      width=3,
+                                      command=self.help)
+        h = Scrollbar(self.root, orient='horizontal')
         h.pack(side=tk.BOTTOM, fill='x')
 
-        self.text = ScrolledText(wrap = tk.NONE, xscrollcommand = h.set)
+        self.text = ScrolledText(wrap=tk.NONE, xscrollcommand=h.set)
         self.frame.pack()
-        self.open_button.pack(side = tk.LEFT)
-        self.help_button.pack(side = tk.RIGHT)
-        self.text.pack(pady = 5, padx = 5)
-        h.config(command = self.text.xview)
+        self.open_button.pack(side=tk.LEFT)
+        self.help_button.pack(side=tk.RIGHT)
+        self.text.pack(pady=5, padx=5)
+        h.config(command=self.text.xview)
         self.root.mainloop()
 
-    def select_file(self):
+    def select_file_xml(self):
         filetypes = (('файлы XML', '*.xml'), ('Все файлы', '*.*'))
-        filenames = fd.askopenfilenames(title='Выбрать файлы', filetypes=filetypes)
+        filenames = fd.askopenfilenames(title='Выбрать файлы',
+                                        filetypes=filetypes)
+        if len(filenames) == 0: return
+        self.start(filenames)
+
+    def select_file_xnc(self):
+        filetypes = (('файлы XNC', '*.xnc'), ('Все файлы', '*.*'))
+        filenames = fd.askopenfilenames(title='Выбрать файлы',
+                                        filetypes=filetypes)
         if len(filenames) == 0: return
         self.start(filenames)
 
     def help(self):
-        message='Конвертер xml файлов в формате KDT в xnc шаблон Giblab. \nАвтор: Тахмазов Борис'
+        message = 'Конвертер xml файлов в формате KDT в xnc шаблон Giblab. \nАвтор: Тахмазов Борис'
         tk.messagebox.showinfo(title='О программе', message=message)
 
     def start(self, filenames):
-        self.text.delete("1.0","end")
+        self.text.delete("1.0", "end")
         for filename in filenames:
-            s = convert(filename)
+            s = convertToKDT(filename)
+            return
             if s == "":
-                self.text.insert(tk.INSERT,os.path.basename(filename)+' - неправильный формат файла\n')
-                #print(f'{filename} - неправильный формат файла')
+                self.text.insert(
+                    tk.INSERT,
+                    os.path.basename(filename) +
+                    ' - неправильный формат файла\n')
+                print(f'{filename} - неправильный формат файла')
                 continue
             file = os.path.splitext(filename)[0]
             with open(file + '.xnc', 'w') as f:
                 f.write(s)
-            self.text.insert(tk.INSERT,os.path.basename(file)+'.xnc - готово\n')
+            self.text.insert(tk.INSERT,
+                             os.path.basename(file) + '.xnc - готово\n')
             #print(f'{filename} - готово')
+
 
 MyApp()
